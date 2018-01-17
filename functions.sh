4be5302dd75cd7555fe1aeb1657ed24124539e52
Mark Shust <mark@shust.com>
2018-01-17 13:16:11 -0500

    Updated print_line_file, added find_replace_file

--- functions.sh
+++ functions.sh
@@ -1,6 +1,12 @@
-# Print a specific line from a file
-# Usage: printlinefromfile 123 foo.txt
-printlinefromfile() {
+# Print a line number from a file
+# Usage: print_line_file 123 foo.txt
+print_line_file() {
   line=$1
   sed $line'q;d' $2
+}
+
+# Find/replace text in a file
+# Usage: find_replace_file foo bar baz.txt
+find_replace_file() {
+  echo -e ",s/$1/$2/g\012 w" | ed -s $3
 }
